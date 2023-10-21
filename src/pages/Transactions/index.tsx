c63b3e812349b23585a443b772111ff1afc83505
patty-ti <rs.patty@outlook.com>
2023-10-21 19:31:30 -0300

    Transactions Table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,37 @@
 import { Header } from "../../components/Header"
 import { Summary } from "../../components/Summary"
 
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles"
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width='50%'>Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 8.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>21/10/2023</td>
+            </tr>
+
+            <tr>
+              <td width='50%'>Sapato</td>
+              <td>
+                <PriceHighlight variant='outcome'>-R$ 159,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/10/2023</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
